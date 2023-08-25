cb6bf2cea3eceeb4d2797737b6728592b175d13f
marhaendev <108604908+marhaendev@users.noreply.github.com>
2023-08-26 01:59:08 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  const copy_info = '<div class="_copy_info"><div class="hasil-salin"></div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check-fill" viewBox="0 0 16 16"><path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z"/><path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585c.055.156.085.325.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5c0-.175.03-.344.085-.5Zm6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708Z"/></svg> COPIED</div>';
+  $('._copy').each(function() {
+    const copy_btn = $('<div class="_copy_btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/> </div>');
+    const $this = $(this);
+    $this.append(copy_btn);
+    copy_btn.click(function() {
+      const htmlToCopy = $this.clone().find('._copy_btn').remove().end().removeClass('_copy').prop('outerHTML');
+      const tempInput = $('<textarea>').val(htmlToCopy);
+      $('body').append(tempInput);
+      tempInput.select();
+      document.execCommand('copy');
+      tempInput.remove();
+      $('._copy_info').remove();
+      copy_btn.after(copy_info);
+      $('._copy_info').fadeIn().delay(1500).fadeOut();
+    });
+  });
+  $('._copy').length > 0 ? $('body').append(copy_info) : null;
+});
